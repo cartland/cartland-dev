d8efc65fc64e010d1e1ccf911084f4b4ce2d264b
cartland <c.cartland@gmail.com>
2025-09-08 22:56:15 -0700

    feat(test): add broken link checker
    Adds a broken link checker to the test suite.
    
    This change introduces a script that starts a local server and crawls the site to find and validate all internal and external links, ensuring there are no 404 errors.
    
    The following packages were added:
    - broken-link-checker
    - http-server
    - start-server-and-test
    - bhttp
    - cheerio
    
    A new 'test:links' script was added to package.json and integrated into the main 'npm test' command.

---
+++ check-links.js
@@ -0,0 +1,39 @@
+const bhttp = require('bhttp');
+const cheerio = require('cheerio');
+const { URL } = require('url');
+
+const siteUrl = 'http://localhost:8080';
+const visited = new Set();
+
+async function checkLink(url) {
+  if (visited.has(url)) {
+    return;
+  }
+  visited.add(url);
+
+  try {
+    const response = await bhttp.get(url);
+    if (response.statusCode >= 400) {
+      console.error(`Broken link: ${url} (${response.statusCode})`);
+      process.exit(1);
+    }
+
+    if (url.startsWith(siteUrl)) {
+      const $ = cheerio.load(response.body);
+      const promises = [];
+      $('a').each((i, link) => {
+        const href = $(link).attr('href');
+        if (href) {
+          const nextUrl = new URL(href, url).href;
+          promises.push(checkLink(nextUrl));
+        }
+      });
+      await Promise.all(promises);
+    }
+  } catch (error) {
+    console.error(`Error checking link: ${url}`, error);
+    process.exit(1);
+  }
+}
+
+checkLink(siteUrl);
